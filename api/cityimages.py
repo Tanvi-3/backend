c80e61e910131ec842665e8483e0000b2dc0e862
DrishyaMody <modydrishya@gmail.com>
2023-10-30 22:32:38 -0700

    Image API to further enhance the webpage

---
+++ api/cityimages.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, jsonify  # jsonify creates an endpoint response object
+from flask_restful import Api, Resource # used for REST API building
+import requests  # used for testing 
+import random
+import http.client
+
+from model.cityimagesmodel import *
+
+cityImage_api = Blueprint('cityImage_api', __name__,
+                   url_prefix='/api/cityimage')
+
+# API generator https://flask-restful.readthedocs.io/en/latest/api.html#id1
+api = Api(cityImage_api)
+
+class CityImageAPI:
+    # not implemented
+    
+    class _Read(Resource):
+        def get(self, cityName):
+            return_dictionary = {"image_url": getCityImage(cityName)}
+            return jsonify(return_dictionary)
+        
+
+    api.add_resource(_Read, '/<string:cityName>')
+
+if __name__ == "__main__": 
+    # server = "http://127.0.0.1:5000" # run local
+    server = 'https://flask.nighthawkcodingsociety.com' # run from web
+    url = server + "/api/cityimage"
+    responses = []  # responses list
+    
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from model.jokes import initJokes
 from model.users import initUsers
 from model.players import initPlayers
 from model.weather2 import initWeather 
+from model.cityimagesmodel import initCityImage
 
 
 #setup APIs
@@ -15,7 +16,7 @@ from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.player import player_api
 from api.weather import weather_api
-
+from api.cityimages import cityImage_api
 
 
 #setup App pages
@@ -31,6 +32,7 @@ app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
 app.register_blueprint(weather_api)
+app.register_blueprint(cityImage_api)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -51,6 +53,7 @@ def activate_job():  # activate these items
     initUsers()
     initPlayers()
     initWeather()
+    initCityImage()
 
 #this runs the application on the development server
 if __name__ == "__main__":
